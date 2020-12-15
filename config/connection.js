dad70a316ae53c8f3855c7454e39e2d1d5325f61
jesscru <jessicacrump7@gmail.com>
2020-12-15 12:01:11 -0700

    added .env and connection.js

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var Sequelize = require("sequelize");
+
+
+var sequelize = new Sequelize(process.env.database, "root", process.env.password, {
+    host: process.env.host,
+    port: process.env.port,
+    dialect: process.env.user,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+
+  module.exports = sequelize;
+
+
+
+// FOR YOUR .ENV FILE:
+
+// host = localhost
+// username = root
+// password = **YOURPASSWORD**
+// database = sneakers
+// port = 3306
