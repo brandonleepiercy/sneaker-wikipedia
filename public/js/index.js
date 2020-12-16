fa0160f5668bcac1a987d9456586af02ffd86fde
Brandon Piercy <b.piercy567@gmail.com>
2020-12-16 13:26:24 -0800

    index skeleton created

--- public/js/index.js
+++ public/js/index.js
@@ -4,4 +4,10 @@ $(function () {
         var searchTerm = $("#search").val().trim();
         //Code to launch the search page
     })
+
+    $("#category-select").on("click", function () {
+        //Store the id of the category clicked by the user on the splash screen
+        var category = $(this).id;
+        //Send the user to the search page and display all results in the category selected by the user
+    });
 })
