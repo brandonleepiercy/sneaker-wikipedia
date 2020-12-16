d261cd2c9accbb77bb0c4e846fce92d0b1cd1ad9
Brandon Piercy <b.piercy567@gmail.com>
2020-12-16 13:08:41 -0800

    created basic skeleton for the index.js file

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#searchbar").on("submit", function () {
+        //Storing the search term from the search box
+        var searchTerm = $("#search").val().trim();
+        //Code to launch the search page
+    })
+})
