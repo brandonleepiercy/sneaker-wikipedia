af78c3c977a3d81bcc5aa94b03ebdb628a61cbbf
Morgan <morganw118@gmail.com>
2020-12-17 13:06:24 -0800

    omg so happy, got the search page to work, hate my life

--- routes/shoes-api-routes.js
+++ routes/shoes-api-routes.js
@@ -13,73 +13,44 @@ module.exports = function(app) {
     });
 
     // finding all shoes belonging to a specific category
-    app.get("/api/shoes/name/:name?/brand/:brand?/release_date/:release_date?/collaborators/:collaborators?/color/:color?/sizing_type/:sizing_type?/sizing_gender/:sizing_gender?/min_size/:min_size?/max_size/:max_size?/price_min/:price_min?/price_max/:price_max?/description/:description?/style/:style?/product_link/:product_link?", function(req, res) {
+    app.post("/api/shoes", function(req, res) {
         let whereProps = {};
 
-        if (req.params.name) {
-            whereProps.name = req.params.name
-        }
-
-        if (req.params.brand) {
-            whereProps.brand = req.params.brand
-        }
-
-        if (req.params.release_date) {
-            whereProps.release_date = req.params.release_date
-        }
-
-        if (req.params.collaborators) {
-            whereProps.collaborators = req.params.collaborators
-        }
-
-        if (req.params.color) {
-            whereProps.color = req.params.color
-        }
-
-        if (req.params.sizing_type) {
-            whereProps.sizing_type = req.params.sizing_type
-        }
-
-        if (req.params.sizing_gender) {
-            whereProps.sizing_gender = req.params.sizing_gender
-        }
-
-        if (req.params.min_size) {
-            whereProps.min_size = req.params.min_size
-        }
-
-        if (req.params.max_size) {
-            whereProps.max_size = req.params.max_size
-        }
-
-        if (req.params.price_min) {
-            whereProps.price_min = {
-                [Op.gte]: req.params.price_min
+        if (req.body.brands) {
+            whereProps.brand = {
+                [Op.or]: req.body.brands
             }
         }
 
-        if (req.params.price_max && price_max <= req.params.price_max) {
-            whereProps.price_max = {
-                [Op.lte]: req.params.price_max
+        if (req.body.collabs) {
+            whereProps.collaborators = {
+                [Op.or]: req.body.collabs
             }
         }
 
-        if (req.params.description) {
-            whereProps.description = req.params.description
+        if (req.body.colors) {
+            whereProps.color = {
+                [Op.or]: req.body.colors
+            }
         }
 
-        if (req.params.style) {
-            whereProps.style = req.params.style
+        if (req.body.gender) {
+            whereProps.sizing_gender = {
+                [Op.or]: req.body.gender
+            }
         }
 
-        if (req.params.product_link) {
-            whereProps.product_link = req.params.product_link
+        if (req.body.styles) {
+            whereProps.style = {
+                [Op.or]: req.body.styles
+            }
         }
 
         
         db.Shoe.findAll({
             where: whereProps
         }).then(function(dbShoe) {
+            console.log(dbShoe);
             res.json(dbShoe);
         });
     });
